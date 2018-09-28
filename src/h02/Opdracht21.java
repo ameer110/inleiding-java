602d99b17165bf5e681dc7485aab607820b4b2a5
Ameer <ameer.dawara@gmail.com>
2018-09-28 10:44:29 +0200

    test nieuwe kleuren

---
+++ src/h02/Opdracht21.java
@@ -0,0 +1,16 @@
+//Voorbeeld 2.2
+
+import java.awt.*;
+import java.applet.*;
+
+public class Opdracht21 extends Applet {
+
+    public void init() {
+        setBackground(Color.blue);
+    }
+
+    public void paint(Graphics g) {
+        g.setColor(Color.yellow);
+        g.drawString("Ameer 11", 50, 60 );
+    }
+}
